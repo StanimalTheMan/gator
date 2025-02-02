cb46b2680466de50d528d22abd4745c8334baf47
Stan Choi <sjc591@nyu.edu>
2025-02-02 09:15:17 -0500

    Add login register commands

--- main.go
+++ main.go
@@ -1,11 +1,51 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/StanimalTheMan/gator/internal/config"
 )
+
+type state struct {
+	config *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	commandsMap map[string]func(*state, command) error
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.commandsMap[name] = f
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	_, ok := c.commandsMap[cmd.name]
+	if ok {
+		c.commandsMap[cmd.name](s, cmd)
+		return nil
+	}
+
+	return errors.New("command does not exist")
+}
+
+func handlerLogin(s *state, cmd command) error {
+	fmt.Println("LOGIN", cmd.args)
+	if len(cmd.args) == 0 {
+		return errors.New("username is required")
+	}
+	fmt.Println("cmd args", cmd.args)
+	s.config.SetUser(cmd.args[0])
+	fmt.Println("username has been set")
+	return nil
+}
 
 func main() {
 	cfg, err := config.Read()
@@ -14,9 +54,32 @@ func main() {
 	}
 	fmt.Printf("Read config: %+v\n", cfg)
 
-	err = cfg.SetUser("stanimal")
+	st := state{config: &cfg}
+	cmds := commands{
+		commandsMap: make(map[string]func(*state, command) error),
+	}
 
-	cfg, err = config.Read()
+	cmds.commandsMap["login"] = handlerLogin
+
+	userArgs := os.Args
+
+	if len(userArgs) < 2 {
+		fmt.Println("fewer than 2 args.  error...")
+		os.Exit(1)
+	}
+
+	cmd := command{
+		userArgs[1],
+		userArgs[2:],
+	}
+
+	err = cmds.commandsMap[userArgs[1]](&st, cmd)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// err = cfg.SetUser("stanimal")
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
