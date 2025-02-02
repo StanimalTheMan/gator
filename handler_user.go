c12a4928e15813075070aac8ef83b541305a8c16
Stan Choi <sjc591@nyu.edu>
2025-02-02 11:20:24 -0500

    Use SQLC to register user using postgres

--- handler_user.go
+++ handler_user.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/StanimalTheMan/gator/internal/database"
+	"github.com/google/uuid"
 )
 
 func handlerLogin(s *state, cmd command) error {
@@ -9,12 +16,58 @@ func handlerLogin(s *state, cmd command) error {
 		return fmt.Errorf("usage: %s <name>", cmd.Name)
 	}
 	name := cmd.Args[0]
+	Context := context.Background()
 
-	err := s.cfg.SetUser(name)
+	// check if user exists before logging in
+	user, err := s.db.GetUser(Context, name)
+	if err != nil {
+		return fmt.Errorf("error occurred while fetching user")
+	}
+	if user.ID == uuid.Nil {
+		log.Fatalf("user not found")
+		os.Exit(1)
+	}
+
+	err = s.cfg.SetUser(name)
 	if err != nil {
 		return fmt.Errorf("couldn't set current user: %w", err)
 	}
 
 	fmt.Println("User switched successfully!")
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <name>", cmd.Name)
+	}
+
+	Context := context.Background()
+	newUserID := uuid.New()
+	name := cmd.Args[0]
+
+	// create user
+	user, err := s.db.CreateUser(Context, database.CreateUserParams{
+		ID:        newUserID,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      name,
+	})
+
+	if err != nil {
+		log.Fatalf("failed to get user")
+		return err
+	}
+	if user.ID == uuid.Nil {
+		log.Fatalf("Name already exists")
+		os.Exit(1)
+	}
+
+	err = s.cfg.SetUser(name)
+	if err != nil {
+		log.Fatalf("couldn't set current user: %v", err)
+		return err
+	}
+	fmt.Printf("user created %v", user)
 	return nil
 }
--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
 	"github.com/StanimalTheMan/gator/internal/config"
+	"github.com/StanimalTheMan/gator/internal/database"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -17,7 +21,14 @@ func main() {
 		log.Fatalf("error reading config: %v", err)
 	}
 
+	db, err := sql.Open("postgres", cfg.DbURL)
+	if err != nil {
+		log.Fatalf("error opening postgres connection")
+	}
+	dbQueries := database.New(db)
+
 	programState := &state{
+		db:  dbQueries,
 		cfg: &cfg,
 	}
 
@@ -25,6 +36,7 @@ func main() {
 		registeredCommands: make(map[string]func(*state, command) error),
 	}
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
